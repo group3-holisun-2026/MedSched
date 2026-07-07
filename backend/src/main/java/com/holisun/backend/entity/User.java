c34f3c6ccd7c62a34555f496d6a38e0741fa3f61
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-07-07 16:34:59 +0300

    Project structure + Enums + User entity

---
+++ backend/src/main/java/com/holisun/backend/entity/User.java
@@ -0,0 +1,43 @@
+package com.holisun.backend.entity;
+
+import com.holisun.backend.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+}
---
+++ backend/src/main/java/com/holisun/backend/enums/NotificationType.java
@@ -0,0 +1,6 @@
+package com.holisun.backend.enums;
+
+public enum NotificationType {
+    EMAIL,
+    SMS
+}
