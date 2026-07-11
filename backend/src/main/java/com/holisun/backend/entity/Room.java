fa0e59440b22447d6b1e6a099efed55baf87cd45
TaschinaLuca <taschina529@gmail.com>
2026-07-11 20:38:58 +0300

    [DB-SCHEMA-002]

---
+++ backend/src/main/java/com/holisun/backend/entity/Room.java
@@ -0,0 +1,23 @@
+package com.holisun.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+}
