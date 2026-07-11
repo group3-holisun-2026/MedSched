af3889ffc01c5aa8d7d831678d0b888e950ea24f
TaschinaLuca <taschina529@gmail.com>
2026-07-11 20:38:58 +0300

    [DB-SCHEMA-002]

---
+++ backend/src/main/java/com/holisun/backend/entity/Doctor.java
@@ -0,0 +1,34 @@
+package com.holisun.backend.entity;
+
+import com.holisun.backend.embeddables.WorkingHours;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="doctors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "speciality", nullable = false, length = 100)
+    private String speciality;
+
+    @Column(name = "standard_consultation_duration_minutes", nullable = false)
+    private Integer standardConsultationDurationMinutes;
+
+    @ElementCollection
+    private List<WorkingHours> weeklySchedule;
+}
