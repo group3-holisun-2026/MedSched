f81c752ee501d0f4f48ebf69a0fe770a9b6e982a
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-07-12 14:46:53 +0300

    created entities and repository
    TODO: migrationV3 for
    Equipment.java
    Room.java
    EquipmentType.java
    Service.java

---
+++ backend/src/main/java/com/holisun/backend/repository/EquipmentRepository.java
@@ -0,0 +1,10 @@
+package com.holisun.backend.repository;
+
+import com.holisun.backend.entity.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, UUID> {
+
+}
