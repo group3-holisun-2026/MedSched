c95d1f1be891e8e24e4bcea50b05b1d50915176f
andrasarmasi <sarmasi.ma.andra@student.utcluj.ro>
2026-07-12 16:48:32 +0300

    login dashboard test

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Dashboard Principal</h1>
+      <p>Aici medicii vor vedea calendarul cu programări.</p>
+    </div>
+  );
+}
