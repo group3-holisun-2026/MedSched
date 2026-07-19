ce79d664104a0f3c9491335c63c9271925b80044
TaschinaLuca <taschina529@gmail.com>
2026-07-19 20:51:59 +0300

    Got rid of everything except V5 migration file

--- backend/src/main/java/com/holisun/backend/entity/Patient.java
+++ backend/src/main/java/com/holisun/backend/entity/Patient.java
@@ -4,7 +4,6 @@ import com.holisun.backend.util.AesEncryptionUtil;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
