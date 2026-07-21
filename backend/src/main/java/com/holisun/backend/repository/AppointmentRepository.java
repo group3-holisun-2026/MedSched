7fb86a2bc06dc8f2ef54efade37a54dfd2d8c695
TaschinaLuca <taschina529@gmail.com>
2026-07-22 00:07:36 +0300

    Fixed: Bugs regarding Appointments,

--- backend/src/main/java/com/holisun/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/holisun/backend/repository/AppointmentRepository.java
@@ -2,15 +2,19 @@ package com.holisun.backend.repository;
 
 import com.holisun.backend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+    @Query
     List<Appointment> findOverlappingForDoctor(UUID doctorId, LocalDateTime start, LocalDateTime end, UUID excludeAppointmentId);
 
+    @Query
     List<Appointment> findOverlappingForRoom(UUID roomId, LocalDateTime start, LocalDateTime end, UUID excludeAppointmentId);
 
+    @Query
     List<Appointment> findOverlappingForEquipment(UUID equipmentId, LocalDateTime start, LocalDateTime end, UUID excludeAppointmentId);
 }
