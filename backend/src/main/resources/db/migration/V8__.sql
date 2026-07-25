75c93df2676041d161e22e11c0d8391453214eb4
TaschinaLuca <taschina529@gmail.com>
2026-07-25 15:10:54 +0300

    Tiny index name modifications
    Modif: Index name

--- backend/src/main/resources/db/migration/V8__.sql
+++ backend/src/main/resources/db/migration/V8__.sql
@@ -1,7 +1,7 @@
 ALTER TABLE appointments
     ADD completed_at TIMESTAMP WITHOUT TIME ZONE;
 
-CREATE INDEX idx_appointments_completed_job
+CREATE INDEX idx_appointments_completed
     ON appointments (status, completed_at)
     WHERE status = 'COMPLETED';
 
