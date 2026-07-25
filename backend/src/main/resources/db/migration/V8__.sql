a67af2b0651dc55005bd1646fd3eb9e5fa7274e1
TaschinaLuca <taschina529@gmail.com>
2026-07-25 15:07:52 +0300

    V8 migration
    Created: Migration V8

---
+++ backend/src/main/resources/db/migration/V8__.sql
@@ -0,0 +1,10 @@
+ALTER TABLE appointments
+    ADD completed_at TIMESTAMP WITHOUT TIME ZONE;
+
+CREATE INDEX idx_appointments_completed_job
+    ON appointments (status, completed_at)
+    WHERE status = 'COMPLETED';
+
+ALTER TABLE users
+DROP
+COLUMN password;
